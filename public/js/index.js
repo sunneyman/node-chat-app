76dafb76abdf9be6e94416a7c1b7697cb695176b
sunneyman <sunney-muz@ukr.net>
2018-08-03 07:19:57 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        to: 'Nick',
-        text: 'Hey, du yo wanna hangoucha?'
-    });
 });
 
 socket.on('disconnect', function () {
